b46501f130f2ad4934bd9678a8aed78d3b5e2db1
David Pochet <david.pochet@gmail.com>
2019-02-21 10:23:05 +0100

    Persist VS Code settings

--- setup.sh
+++ setup.sh
@@ -4,18 +4,37 @@ set -e
 # Get root folder from current script
 ROOTSCRIPT="$(readlink -f "$0")"
 ROOTDIR="$(dirname "$ROOTSCRIPT")"
-SHELLSCRIPTS="${ROOTDIR}/shell"
 
-# Shell: loop on provided files
-for SCRIPT_FILE in $(cd ${SHELLSCRIPTS}; find -type f | sed -e 's|./||g'); do
-    # Backup files if they exist
-    if test -f "${HOME}/${SCRIPT_FILE}" -a ! -L "${HOME}/${SCRIPT_FILE}"; then
-        echo "Backuping ${HOME}/${SCRIPT_FILE} to ${HOME}/${SCRIPT_FILE}.save"
-        mv "${HOME}/${SCRIPT_FILE}" "${HOME}/${SCRIPT_FILE}.save"
+# Update settings function
+function __updateSettings {
+    # Settings folder to browse
+    local SETTINGS_FOLDER="$1"
+    shift
+
+    # Target folder to update
+    local TARGET_FOLDER="$1"
+    shift
+
+    # Perform settings links only if target folder exists
+    if test -d "${TARGET_FOLDER}"; then
+        # Loop on provided files
+        for SETTINGS_FILE in $(cd ${SETTINGS_FOLDER}; find -type f | sed -e 's|./||g'); do
+            # Backup files if they exist
+            if test -f "${TARGET_FOLDER}/${SETTINGS_FILE}" -a ! -L "${TARGET_FOLDER}/${SETTINGS_FILE}"; then
+                echo "Backuping ${TARGET_FOLDER}/${SETTINGS_FILE} to ${TARGET_FOLDER}/${SETTINGS_FILE}.save"
+                mv "${TARGET_FOLDER}/${SETTINGS_FILE}" "${TARGET_FOLDER}/${SETTINGS_FILE}.save"
+            fi
+
+            # Then link the ones from this repo
+            rm -f "${TARGET_FOLDER}/${SETTINGS_FILE}"
+            echo "Linking ${SETTINGS_FOLDER}/${SETTINGS_FILE} in ${TARGET_FOLDER}"
+            ln -nfsr "${SETTINGS_FOLDER}/${SETTINGS_FILE}" "${TARGET_FOLDER}/${SETTINGS_FILE}"
+        done
     fi
+}
 
-    # Then link the ones from this repo
-    rm -f "${HOME}/${SCRIPT_FILE}"
-    echo "Linking ${SHELLSCRIPTS}/${SCRIPT_FILE}"
-    ln -nfsr "${SHELLSCRIPTS}/${SCRIPT_FILE}" "${HOME}/${SCRIPT_FILE}"
-done
+# Shell profiles
+__updateSettings "${ROOTDIR}/shell" "${HOME}"
+
+# VS Code settings
+__updateSettings "${ROOTDIR}/vscode" "${HOME}/.config/Code/User"
