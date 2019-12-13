80f0cd3780467e12e3f0151e66eadf6a49fd423f
David Pochet <david.pochet@gmail.com>
2019-12-13 14:20:33 +0100

    Restore terminal window title in PS1

--- shell/profile.d/10-prompt.sh
+++ shell/profile.d/10-prompt.sh
@@ -1,3 +1,3 @@
 # Custom path with hour and last command result
 # See http://bashrcgenerator.com/ for update
-export PS1="\[$(tput bold)\]\[\033[38;5;10m\]\u@\h\[\033[38;5;15m\]:\[\033[01;34m\]\w\[$(tput sgr0)\] \[\033[38;5;11m\]\A \[\033[38;5;9m\][\$?]\[\033[38;5;15m\] \\$\[$(tput sgr0)\] "
+export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[$(tput bold)\]\[\033[38;5;10m\]\u@\h\[\033[38;5;15m\]:\[\033[01;34m\]\w\[$(tput sgr0)\] \[\033[38;5;11m\]\A \[\033[38;5;9m\][\$?]\[\033[38;5;15m\] \\$\[$(tput sgr0)\] "
