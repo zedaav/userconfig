77894d075caa444c60dc62d6e0aca8983a2ca62b
David Pochet <david.pochet@gmail.com>
2020-05-05 09:21:26 +0200

    Handle not supported docker context command

--- shell/profile.d/15-docker-context.sh
+++ shell/profile.d/15-docker-context.sh
@@ -2,7 +2,7 @@
 if __has docker; then
     # Get Docker context
     function __updatedockerContext {
-        DK_CONTEXT="$(docker context ls | grep ' \*' | cut -d ' ' -f 1)"
+        DK_CONTEXT="$(docker context ls 2> /dev/null | grep ' \*' | cut -d ' ' -f 1)"
         if test "${DK_CONTEXT}" == "default"; then
             # In default context, nothing to display
             DK_CONTEXT=""
