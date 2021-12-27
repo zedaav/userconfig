adb5f7149cca6eb171cb6124bd6bcc7ffe4bab9f
David Pochet <david.pochet@gmail.com>
2021-12-27 16:41:13 +0100

    Faster docker context computing

--- shell/profile.d/15-docker-context.sh
+++ shell/profile.d/15-docker-context.sh
@@ -2,11 +2,7 @@
 if __has docker; then
     # Get Docker context
     function __updatedockerContext {
-        DK_CONTEXT="$(docker context ls 2> /dev/null | grep ' \*' | cut -d ' ' -f 1)"
-        if test "${DK_CONTEXT}" == "default"; then
-            # In default context, nothing to display
-            DK_CONTEXT=""
-        fi
+        DK_CONTEXT="$(echo $DOCKER_HOST | sed -e "s@tcp://@@g")"
     }
 else
     # Stubs
